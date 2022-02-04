5b01e33a7955c40e760e126eea3468892c5c6d8d
Dayrons <dayronstovar@gmail.com>
2022-02-04 13:42:34 -0400

    Diseño de barra de busqueda(sin funcionalidad)

--- lib/pages/inicio/pagina_usuario.dart
+++ lib/pages/inicio/pagina_usuario.dart
@@ -3,6 +3,7 @@ import 'package:app/pages/inicio/widgets/notificacion.dart';
 import 'package:app/pages/registrar_password/pagina_registrar_password.dart';
 import 'package:app/pages/inicio/widgets/password.dart';
 import 'package:app/utils/ui.dart';
+import 'package:app/widgets/input.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -86,57 +87,85 @@ class _PaginaInicioState extends State<PaginaInicio> {
         ),
         backgroundColor: Color(0XFF1c1d22),
       ),
-      body: BlocBuilder<GestionpasswordBloc, GestionpasswordState>(
-          builder: (context, state) {
-        if (state.obteniendoPassword) {
-          return Text('Obteniendo password');
-        } else {
-          if (state.passwords.length > 0) {
-            return Stack(children: [
-              ListView.builder(
-                itemCount: state.passwords.length,
-                itemBuilder: (BuildContext context, int index) {
-                  final password = state.passwords[index];
-                  return Password(
-                    titulo: password.titulo,
-                    password: password.decryptFernet(password.password),
-                    funcion: _notificacion,
-                  );
-                },
-              ),
-              Positioned(
-                top: 10.00,
-                left: size.width / 2 - 120.00 / 2,
-                width: 120.00,
-                height: 35.00,
-                child: Notificacion(opacidad: opacidad),
-              ),
-            ]);
-          } else {
-            return SizedBox(
-              width: double.infinity,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Image.asset(
-                    'assets/4.png',
-                    width: 140,
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.only(
+                left: 20.00, right: 20.0, bottom: 40.00, top: 20.00),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20.00),
+              color: Color(0XFF2b2e3d).withOpacity(0.5),
+            ),
+            child: TextField(
+              style: TextStyle(color: Colors.white),
+              cursorColor: Color(0XFF2CDA9D),
+              decoration: InputDecoration(
+                  fillColor: Colors.white,
+                  border: InputBorder.none,
+                  hintText: "Buscar",
+                  hintStyle: TextStyle(color: Colors.white),
+                  prefixIcon: Icon(
+                    Icons.search,
+                    color: Colors.white,
+                  )),
+              onChanged: (text) {},
+            ),
+          ),
+          BlocBuilder<GestionpasswordBloc, GestionpasswordState>(
+              builder: (context, state) {
+            if (state.obteniendoPassword) {
+              return Text('Obteniendo password');
+            } else {
+              if (state.passwords.length > 0) {
+                return Expanded(
+                  child: Stack(children: [
+                    ListView.builder(
+                      itemCount: state.passwords.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        final password = state.passwords[index];
+                        return Password(
+                          titulo: password.titulo,
+                          password: password.decryptFernet(password.password),
+                          funcion: _notificacion,
+                        );
+                      },
+                    ),
+                    Positioned(
+                      top: 10.00,
+                      left: size.width / 2 - 120.00 / 2,
+                      width: 120.00,
+                      height: 35.00,
+                      child: Notificacion(opacidad: opacidad),
+                    ),
+                  ]),
+                );
+              } else {
+                return SizedBox(
+                  width: double.infinity,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        'assets/4.png',
+                        width: 140,
+                      ),
+                      SizedBox(
+                        height: 20.00,
+                      ),
+                      Text('No tienes ninguna contraseña',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16.00))
+                    ],
                   ),
-                  SizedBox(
-                    height: 20.00,
-                  ),
-                  Text('No tienes ninguna contraseña',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16.00))
-                ],
-              ),
-            );
-          }
-        }
-      }),
+                );
+              }
+            }
+          })
+        ],
+      ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
